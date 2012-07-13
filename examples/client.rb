ffb4acd6cc1c5800231033aa8028fc25bcd93f92
Bernard Potocki <bernard.potocki@imanel.org>
2012-07-13 11:27:39 +0200

    Add client example and extract common part

---
+++ examples/client.rb
@@ -0,0 +1,49 @@
+require './base'
+
+# Example WebSocket Client (using EventMachine)
+# @example Usage
+#   ws = WebSocket::EventMachine::Client.connect(:host => "0.0.0.0", :port => 8080)
+#   ws.onmessage { |msg| ws.send "Pong: #{msg}" }
+#   ws.send "data"
+module WebSocket
+  module EvenMachine
+    class Client < Base
+
+      # Connect to websocket server
+      # @param args [Hash] The request arguments
+      # @option args [String] :host The host IP/DNS name
+      # @option args [Integer] :port The port to connect too(default = 80)
+      # @option args [Integer] :version Version of protocol to use(default = 13)
+      def self.connect(args = {})
+        args[:port] ||= 80
+        EventMachine.connect args[:host], args[:port], self, args
+      end
+
+      # Initialize connection
+      # @param args [Hash] Arguments for connection
+      # @option args [String] :host The host IP/DNS name
+      # @option args [Integer] :port The port to connect too(default = 80)
+      # @option args [Integer] :version Version of protocol to use(default = 13)
+      def initialize(args)
+        @args = args
+      end
+
+      ############################
+      ### EventMachine methods ###
+      ############################
+
+      # Called after initialize of connection, but before connecting to server
+      def post_init
+        @state = :connecting
+        @handshake = WebSocket::Handshake::Client.new(@args)
+      end
+
+      # Called by EventMachine after connecting.
+      # Sends handshake to server
+      def connection_completed
+        send(@handshake.to_s, :type => :plain)
+      end
+
+    end
+  end
+end
--- examples/server.rb
+++ examples/server.rb
@@ -1,173 +1,44 @@
+require './base'
+
 # Example WebSocket Server (using EventMachine)
+# @example Usage
+#   WebSocket::Example::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
+#     ws.onopen    { ws.send "Hello Client!"}
+#     ws.onmessage { |msg| ws.send "Pong: #{msg}" }
+#     ws.onclose   { puts "WebSocket closed" }
+#     ws.onerror   { |e| puts "Error: #{e}" }
+#   end
 module WebSocket
-  module Server
+  module EventMachine
+    class Server < Base
 
-    # Start server
-    # Usage:
-    #   WebSocket::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
-    #     ws.onopen    { ws.send "Hello Client!"}
-    #     ws.onmessage { |msg| ws.send "Pong: #{msg}" }
-    #     ws.onclose   { puts "WebSocket closed" }
-    #     ws.onerror   { |e| puts "Error: #{e}" }
-    #   end
-    def self.start(options, &block)
-      EM.epoll
-      EM.run do
-
-        trap("TERM") { stop }
-        trap("INT")  { stop }
-
-        EventMachine::start_server(options[:host], options[:port],
-          WebSocket::Server::Connection, options) do |c|
+      # Start server
+      # @param options [Hash] The request arguments
+      # @option args [String] :host The host IP/DNS name
+      # @option args [Integer] :port The port to connect too(default = 80)
+      def self.start(options, &block)
+        EventMachine::start_server(options[:host], options[:port], self, options) do |c|
           blk.call(c)
         end
       end
-    end
 
-    class Connection < EventMachine::Connection
-
-      ###########
-      ### API ###
-      ###########
-
-      def onopen(&blk);     @onopen = blk;    end # Called when connection is opened
-      def onclose(&blk);    @onclose = blk;   end # Called when connection is closed
-      def onerror(&blk);    @onerror = blk;   end # Called when error occurs
-      def onmessage(&blk);  @onmessage = blk; end # Called when message is received from client
-      def onping(&blk);     @onping = blk;    end # Called when ping message is received from client
-      def onpong(&blk);     @onpong = blk;    end # Called when pond message is received from client
-
-      # Send data to client
-      # Return true if data was send, otherwise call on_error if needed.
-      def send(data, options = {})
-        type = options[:type] || :text
-        unless type == :plain
-          frame = WebSocket::Frame::Outgoing.new(:version => @handshake.version, :data => data, :type => type)
-          if !frame.supported?
-            trigger_onerror("Frame type '#{type}' is not supported in protocol version #{@handshake.version}")
-            return false
-          elsif !frame.require_sending?
-            return false
-          end
-          data = frame.to_binary
-        end
-        send_data(data)
-        true
-      end
-
-      # Close connection
-      # Return true if connection is closed immediately
-      def close
-        return false if @state == :open && send('', :type => :close)
-        @state = :closed
-        close_connection
-        true
-      end
-
-      # Send ping message to client
-      # Return false if protocol version is not supporting ping requests
-      def ping(data = '')
-        send(data, :type => :ping)
-      end
-
-      # Send pong message to client
-      # Return false if protocol version is not supporting pong requests.
-      def pong(data = '')
-        send(data, :type => :pong)
+      # Initialize connection
+      # @param args [Hash] Arguments for server
+      # @option args [Boolean] :secure If true then server will run over SSL
+      # @option args [Hash] :tls_options Options for SSL if secure = true
+      def initialize(args)
+        @secure = args[:secure] || false
+        @tls_options = args[:tls_options] || {}
       end
 
       ############################
       ### Eventmachine methods ###
       ############################
 
-      def initialize(options)
-        @options = options
-        @secure = options[:secure] || false
-        @tls_options = options[:tls_options] || {}
-
+      def post_init
         @state = :connecting
         @handshake = WebSocket::Handshake::Server.new
-      end
-
-      def post_init
         start_tls(@tls_options) if @secure
-      end
-
-      def receive_data(data)
-        case @state
-        when :connecting then handle_connecting(data)
-        when :open then handle_open(data)
-        when :closing then handle_closing(data)
-        end
-      end
-
-      def unbind
-        unless @state == :closed
-          @state = :closed
-          close
-          trigger_onclose
-        end
-      end
-
-      #######################
-      ### Private methods ###
-      #######################
-
-      private
-
-      ['onopen', 'onclose'].each do |m|
-        define_method "trigger_#{m}" do
-          callback = instance_variable_get("@#{m}")
-          callback.call if callback
-        end
-      end
-
-      ['onerror', 'onmessage', 'onping', 'onpong'].each do |m|
-        define_method "trigger_#{m}" do |data|
-          callback = instance_variable_get("@#{m}")
-          callback.call(data) if callback
-        end
-      end
-
-      def handle_connecting(data)
-        @handshake << data
-        return unless @handshake.finished?
-        if @handshake.valid?
-          send(@handshake.response.to_s, :type => :plain)
-          @frame = WebSocket::Frame::Incoming.new(:version => @handshake.version)
-          @state = :open
-          trigger_onopen
-        else
-          trigger_onerror(@handshake.error)
-          close
-        end
-      end
-
-      def handle_open(data)
-        @frame << data
-        while frame = @frame.next
-          case frame.type
-          when :close
-            @state = :closing
-            close
-            trigger_onclose
-          when :ping
-            pong(frame.to_s)
-            trigger_onping(frame.to_s)
-          when :pong
-            trigger_onpong(frame.to_s)
-          when :text
-            trigger_onmessage(frame.to_s)
-          when :binary
-            trigger_onmessage(frame.to_binary)
-          end
-        end
-      end
-
-      def handle_closing(data)
-        @state = :closed
-        close
-        trigger_onclose
       end
 
     end
