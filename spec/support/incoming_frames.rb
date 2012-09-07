85c5c4757bb1cbaf9e11489eb26db0c73c9f4f28
Bernard Potocki <bernard.potocki@imanel.org>
2012-09-07 11:40:50 +0200

    Make incoming frame spec more DRY

--- spec/support/incoming_frames.rb
+++ spec/support/incoming_frames.rb
@@ -1,21 +1,28 @@
 shared_examples_for 'valid_incoming_frame' do
+  let(:decoded_text_array) { Array(decoded_text) }
+  let(:frame_type_array) { Array(frame_type) }
+
   its(:class) { should eql(WebSocket::Frame::Incoming) }
+  its(:data) { should eql(encoded_text || "") }
   its(:version) { should eql(version) }
-  its(:type) { should eql(frame_type) }
-  its(:error) { should be_nil }
-  its(:next) { should be_nil }
-end
-
-shared_examples_for 'valid_encoded_frame' do
-  it_should_behave_like 'valid_incoming_frame'
-
+  its(:type) { should be_nil }
   its(:decoded?) { should be_false }
-  its(:to_s) { should eql(encoded_text) }
-end
+  its(:to_s) { should eql(encoded_text || "") }
 
-shared_examples_for 'valid_decoded_frame' do
-  it_should_behave_like 'valid_incoming_frame'
+  it "should have specified number of returned frames" do
+    decoded_text_array.each_with_index do |da, index|
+      subject.next.should_not be_nil, "Should return frame for #{da}, #{frame_type_array[index]}"
+    end
+    subject.next.should be_nil
+    subject.error.should eql(error)
+  end
 
-  its(:decoded?) { should be_true }
-  its(:to_s) { should eql(decoded_text) }
+  it "should return valid decoded frame for each specified decoded texts" do
+    decoded_text_array.each_with_index do |da, index|
+      f = subject.next
+      f.decoded?.should be_true
+      f.type.should eql(frame_type_array[index])
+      f.to_s.should eql(da)
+    end
+  end
 end
