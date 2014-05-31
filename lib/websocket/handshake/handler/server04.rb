42cb0bda63ae9814e73920bed8dd74b44b4a6818
Bernard Potocki <bernard.potocki@imanel.org>
2014-05-31 12:16:54 +0200

    Simplify this code

--- lib/websocket/handshake/handler/server04.rb
+++ lib/websocket/handshake/handler/server04.rb
@@ -30,13 +30,18 @@ module WebSocket
         # Signature of response, created from client request Sec-WebSocket-Key
         # @return [String] signature
         def signature
-          return unless key = @handshake.headers['sec-websocket-key']
+          return unless key
           string_to_sign = "#{key}258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
           Base64.encode64(Digest::SHA1.digest(string_to_sign)).chomp
         end
 
         def verify_key
-          (@handshake.headers['sec-websocket-key'] ? true : raise(WebSocket::Error::Handshake::InvalidAuthentication))
+          raise WebSocket::Error::Handshake::InvalidAuthentication unless key
+          true
+        end
+
+        def key
+          @handshake.headers['sec-websocket-key']
         end
 
       end
