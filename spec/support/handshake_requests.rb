b61125acc7743a4baf5f175c5316d2da1bd3af94
Bernard Potocki <bernard.potocki@imanel.org>
2018-05-25 17:15:23 +0200

    Rubocop: Naming/HeredocDelimiterNaming

--- spec/support/handshake_requests.rb
+++ spec/support/handshake_requests.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
 def client_handshake_75(args = {})
-  <<-EOF
+  <<-REQUEST
 GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
 Upgrade: WebSocket\r
 Connection: Upgrade\r
 Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
 Origin: http://example.com\r
 #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}\r
-  EOF
+  REQUEST
 end
 
 def server_handshake_75(args = {})
-  <<-EOF
+  <<-REQUEST
 HTTP/1.1 101 Web Socket Protocol Handshake\r
 Upgrade: WebSocket\r
 Connection: Upgrade\r
 #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}WebSocket-Origin: http://example.com\r
 WebSocket-Location: ws#{args[:secure] ? 's' : ''}://#{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}#{args[:path] || '/demo'}\r
 \r
-  EOF
+  REQUEST
 end
 
 def client_handshake_76(args = {})
-  request = <<-EOF
+  request = <<-REQUEST
 GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
 Upgrade: WebSocket\r
 Connection: Upgrade\r
@@ -33,12 +33,12 @@ Origin: http://example.com\r
 Sec-WebSocket-Key2: #{args[:key2] || '12998 5 Y3 1  .P00'}\r
 \r
 #{args[:key3] || '^n:ds[4U'}
-  EOF
+  REQUEST
   request[0..-2]
 end
 
 def server_handshake_76(args = {})
-  request = <<-EOF
+  request = <<-REQUEST
 HTTP/1.1 101 WebSocket Protocol Handshake\r
 Upgrade: WebSocket\r
 Connection: Upgrade\r
@@ -46,12 +46,12 @@ Connection: Upgrade\r
 Sec-WebSocket-Location: ws#{args[:secure] ? 's' : ''}://#{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}#{args[:path] || '/demo'}\r
 \r
 #{args[:challenge] || "8jKS'y:G*Co,Wxa-"}
-  EOF
+  REQUEST
   request[0..-2]
 end
 
 def client_handshake_04(args = {})
-  <<-EOF
+  <<-REQUEST
 GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
 Upgrade: websocket\r
 Connection: Upgrade\r
@@ -60,21 +60,21 @@ Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
 Sec-WebSocket-Version: #{args[:version] || '4'}\r
 Sec-WebSocket-Key: #{args[:key] || 'dGhlIHNhbXBsZSBub25jZQ=='}\r
 \r
-  EOF
+  REQUEST
 end
 
 def server_handshake_04(args = {})
-  <<-EOF
+  <<-REQUEST
 HTTP/1.1 101 Switching Protocols\r
 Upgrade: websocket\r
 Connection: Upgrade\r
 #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Accept: #{args[:accept] || 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='}\r
 \r
-  EOF
+  REQUEST
 end
 
 def client_handshake_11(args = {})
-  <<-EOF
+  <<-REQUEST
 GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
 Upgrade: websocket\r
 Connection: Upgrade\r
@@ -83,7 +83,7 @@ Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
 Sec-WebSocket-Version: #{args[:version] || '4'}\r
 Sec-WebSocket-Key: #{args[:key] || 'dGhlIHNhbXBsZSBub25jZQ=='}\r
 \r
-  EOF
+  REQUEST
 end
 
 def server_handshake_11(args = {})
