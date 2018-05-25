10374c9cf047b84ecc2ba1c8b734a89d344faf15
Bernard Potocki <bernard.potocki@imanel.org>
2018-05-25 17:01:33 +0200

    Revert "Updates for Rubocop"
    This reverts commit edb6c88fb7cd9cc97b3fcd7afdcfe6010c061660.

--- spec/support/handshake_requests.rb
+++ spec/support/handshake_requests.rb
@@ -1,89 +1,89 @@
 # frozen_string_literal: true
 
 def client_handshake_75(args = {})
-  <<~REQUEST
-    GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
-    Upgrade: WebSocket\r
-    Connection: Upgrade\r
-    Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
-    Origin: http://example.com\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}\r
-  REQUEST
+  <<-EOF
+GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
+Upgrade: WebSocket\r
+Connection: Upgrade\r
+Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
+Origin: http://example.com\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}\r
+  EOF
 end
 
 def server_handshake_75(args = {})
-  <<~REQUEST
-    HTTP/1.1 101 Web Socket Protocol Handshake\r
-    Upgrade: WebSocket\r
-    Connection: Upgrade\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}WebSocket-Origin: http://example.com\r
-    WebSocket-Location: ws#{args[:secure] ? 's' : ''}://#{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}#{args[:path] || '/demo'}\r
-    \r
-  REQUEST
+  <<-EOF
+HTTP/1.1 101 Web Socket Protocol Handshake\r
+Upgrade: WebSocket\r
+Connection: Upgrade\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}WebSocket-Origin: http://example.com\r
+WebSocket-Location: ws#{args[:secure] ? 's' : ''}://#{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}#{args[:path] || '/demo'}\r
+\r
+  EOF
 end
 
 def client_handshake_76(args = {})
-  request = <<~REQUEST
-    GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
-    Upgrade: WebSocket\r
-    Connection: Upgrade\r
-    Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
-    Origin: http://example.com\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Key1: #{args[:key1] || '4 @1  46546xW%0l 1 5'}\r
-    Sec-WebSocket-Key2: #{args[:key2] || '12998 5 Y3 1  .P00'}\r
-    \r
-    #{args[:key3] || '^n:ds[4U'}
-  REQUEST
+  request = <<-EOF
+GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
+Upgrade: WebSocket\r
+Connection: Upgrade\r
+Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
+Origin: http://example.com\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Key1: #{args[:key1] || '4 @1  46546xW%0l 1 5'}\r
+Sec-WebSocket-Key2: #{args[:key2] || '12998 5 Y3 1  .P00'}\r
+\r
+#{args[:key3] || '^n:ds[4U'}
+  EOF
   request[0..-2]
 end
 
 def server_handshake_76(args = {})
-  request = <<~REQUEST
-    HTTP/1.1 101 WebSocket Protocol Handshake\r
-    Upgrade: WebSocket\r
-    Connection: Upgrade\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Origin: http://example.com\r
-    Sec-WebSocket-Location: ws#{args[:secure] ? 's' : ''}://#{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}#{args[:path] || '/demo'}\r
-    \r
-    #{args[:challenge] || "8jKS'y:G*Co,Wxa-"}
-  REQUEST
+  request = <<-EOF
+HTTP/1.1 101 WebSocket Protocol Handshake\r
+Upgrade: WebSocket\r
+Connection: Upgrade\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Origin: http://example.com\r
+Sec-WebSocket-Location: ws#{args[:secure] ? 's' : ''}://#{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}#{args[:path] || '/demo'}\r
+\r
+#{args[:challenge] || "8jKS'y:G*Co,Wxa-"}
+  EOF
   request[0..-2]
 end
 
 def client_handshake_04(args = {})
-  <<~REQUEST
-    GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
-    Upgrade: websocket\r
-    Connection: Upgrade\r
-    Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Origin: http://example.com\r
-    Sec-WebSocket-Version: #{args[:version] || '4'}\r
-    Sec-WebSocket-Key: #{args[:key] || 'dGhlIHNhbXBsZSBub25jZQ=='}\r
-    \r
-  REQUEST
+  <<-EOF
+GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
+Upgrade: websocket\r
+Connection: Upgrade\r
+Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Origin: http://example.com\r
+Sec-WebSocket-Version: #{args[:version] || '4'}\r
+Sec-WebSocket-Key: #{args[:key] || 'dGhlIHNhbXBsZSBub25jZQ=='}\r
+\r
+  EOF
 end
 
 def server_handshake_04(args = {})
-  <<~REQUEST
-    HTTP/1.1 101 Switching Protocols\r
-    Upgrade: websocket\r
-    Connection: Upgrade\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Accept: #{args[:accept] || 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='}\r
-    \r
-  REQUEST
+  <<-EOF
+HTTP/1.1 101 Switching Protocols\r
+Upgrade: websocket\r
+Connection: Upgrade\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Sec-WebSocket-Accept: #{args[:accept] || 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='}\r
+\r
+  EOF
 end
 
 def client_handshake_11(args = {})
-  <<~REQUEST
-    GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
-    Upgrade: websocket\r
-    Connection: Upgrade\r
-    Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
-    #{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Origin: http://example.com\r
-    Sec-WebSocket-Version: #{args[:version] || '4'}\r
-    Sec-WebSocket-Key: #{args[:key] || 'dGhlIHNhbXBsZSBub25jZQ=='}\r
-    \r
-  REQUEST
+  <<-EOF
+GET #{args[:path] || '/demo'}#{"?#{args[:query]}" if args[:query]} HTTP/1.1\r
+Upgrade: websocket\r
+Connection: Upgrade\r
+Host: #{args[:host] || 'example.com'}#{":#{args[:port]}" if args[:port]}\r
+#{(args[:headers] || {}).map { |key, value| "#{key}: #{value}\r\n" }.join('')}Origin: http://example.com\r
+Sec-WebSocket-Version: #{args[:version] || '4'}\r
+Sec-WebSocket-Key: #{args[:key] || 'dGhlIHNhbXBsZSBub25jZQ=='}\r
+\r
+  EOF
 end
 
 def server_handshake_11(args = {})
--- websocket.gemspec
+++ websocket.gemspec
@@ -19,5 +19,5 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map { |f| File.basename(f) }
   s.require_paths = ['lib']
 
-  s.required_ruby_version = '>= 2.3'
+  s.required_ruby_version = '>= 2.0'
 end
